3cac3c1437ccc2a4cdeff57f84da620c7242f360
Vincent-Vais <vincentvais17@gmail.com>
2020-08-23 00:39:17 -0500

    change headers

--- amazon_bs.py
+++ amazon_bs.py
@@ -40,7 +40,7 @@ logger.disabled = True
 def setup():
     logger.info("Entered Setup")
     headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"
     }
     logger.debug(f"Created headers: {headers}")
     PROXY = getProxy()
